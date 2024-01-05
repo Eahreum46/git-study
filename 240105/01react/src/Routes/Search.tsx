69be0fa148f5879eab391b3d17dcb1e66b47b62e
Eahreum46 <dkfma4466@hanmail.net>
2024-01-05 15:23:04 +0900

    240105

---
+++ 240105/01react/src/Routes/Search.tsx
@@ -0,0 +1,12 @@
+import { useLocation } from "react-router-dom";
+
+const Search = () => {
+  const location = useLocation();
+  console.log(location);
+
+  const search = new URLSearchParams(location.search).get("keyword");
+  console.log(search);
+  return <div></div>;
+};
+
+export default Search;
---
+++ 240105/01react/src/api.ts
@@ -0,0 +1,24 @@
+const API_KEY = "0694c406aa2947fb492511925eefa84c";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: { maximum: string; minimum: string };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export const getMovies = () => {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+};
